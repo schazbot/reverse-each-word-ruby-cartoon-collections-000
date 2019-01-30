653aef67b24f941bebc99268d692d28233ca3f15
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-30 17:12:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ sentence = "Hello there, and how are you?"
 
 
 def reverse_each_word(sentence)
-new_sentence = sentence.split(/ /)
-new_sentence.each do |x|
+new_sentence = sentence.split(/ /).each do |x|
 x.reverse!
 end
+new_sentence.join(" ")
 end
